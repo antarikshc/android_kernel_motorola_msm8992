65df57631f77f826a4a7c52af3a622409609d299
Sahitya Tummala <stummala@codeaurora.org>
2015-10-09 13:30:46 +0530

    mmc: core: revert recent changes in shutdown handling code
    This change reverts the following gerrits as they break the
    existing shutdown functionality for eMMC and SD card.
    
    8635767 "mmc: core: Extend shutdown sequence to handle bus operations"
    b4f9379 "mmc: core: Add shutdown callback for (e)MMC bus_ops"
    e3126b8 "mmc: core: Add shutdown callback for SD bus_ops"
    
    Change-Id: I3d34404e653600d52015afe851ae55b7e60e07dd
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>

--- drivers/mmc/core/sd.c
+++ drivers/mmc/core/sd.c
@@ -1398,7 +1398,6 @@ static const struct mmc_bus_ops mmc_sd_ops = {
 	.power_restore = mmc_sd_power_restore,
 	.alive = mmc_sd_alive,
 	.change_bus_speed = mmc_sd_change_bus_speed,
-	.shutdown = mmc_sd_suspend,
 	.throttle_back = mmc_sd_throttle_back,
 };
 
@@ -1410,7 +1409,6 @@ static const struct mmc_bus_ops mmc_sd_ops_unsafe = {
 	.power_restore = mmc_sd_power_restore,
 	.alive = mmc_sd_alive,
 	.change_bus_speed = mmc_sd_change_bus_speed,
-	.shutdown = mmc_sd_suspend,
 	.throttle_back = mmc_sd_throttle_back,
 };
 
