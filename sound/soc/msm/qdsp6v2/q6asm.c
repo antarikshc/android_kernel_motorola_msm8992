8accd73f10f774c459faef9dcbe29fb5fae8e6c1
Laxminath Kasam <lkasam@codeaurora.org>
2016-02-22 12:21:54 +0530

    ASoC: msm: qdsp6v2: Move spin lock init before apr registration
    Move spin lock init for no wait que before apr registration.
    If apr registration fails asm free client will free
    the no wait que by acquiring spin lock. Hence spin lock
    init for no wait que should happen before apr registration.
    
    CRs-Fixed: 979701
    Change-Id: I5ef85e39ab5e3babfb2e097005c0d1553cc38f51
    Signed-off-by: Laxminath Kasam <lkasam@codeaurora.org>

--- sound/soc/msm/qdsp6v2/q6asm.c
+++ sound/soc/msm/qdsp6v2/q6asm.c
@@ -989,6 +989,7 @@ struct audio_client *q6asm_audio_client_alloc(app_cb cb, void *priv)
 	/* DSP expects stream id from 1 */
 	ac->stream_id = 1;
 	INIT_LIST_HEAD(&ac->no_wait_que);
+	spin_lock_init(&ac->no_wait_que_spinlock);
 	ac->apr = apr_register("ADSP", "ASM", \
 			(apr_fn)q6asm_callback,\
 			((ac->session) << 8 | 0x0001),\
